5b10f6de4dfd7df147e126de2b60cdf854cb289c
IsraelSantosV <IsraelS_Vieira@hotmail.com>
2022-04-08 07:20:25 -0300

    Reset application

--- src/resources/Animator.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Israel on 22/03/2022.
-//
-
-#include "../../include/vox-engine/resources/Animator.h"
--- src/util/MaxQueue.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by zetta on 30/03/2022.
-//
-
-#include "../../include/vox-engine/util/MaxQueue.h"
